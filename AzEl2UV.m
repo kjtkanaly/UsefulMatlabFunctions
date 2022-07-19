d672be631f42330b406f6435678d2a48ac49e059
kjtkanaly <54219067+kjtkanaly@users.noreply.github.com>
2022-07-19 15:21:00 -0400

    Add files via upload

---
+++ AzEl2UV.m
@@ -0,0 +1,14 @@
+function [u, v, w] = AzEl2UV(Az, El, tiltAz, tiltEl)
+
+	% Convert to Radians
+	Az = Az .* pi ./ 180;
+	El = El .* pi ./ 180;
+	tiltAz = tiltAz .* pi ./ 180;
+	tiltEl = tiltEl .* pi ./ 180;
+	
+	% Convert to UV
+	u = cos(El) .* sin(tiltAz - Az);
+	v = sin(El - tiltEl) + cos(El) .* sin(tiltEl) .* (1 - cos(tiltAz - Az));
+	w = sin(El) .* sin(tiltEl) + cos(El) .* cos(tiltEl) .* cos(tiltAz - Az);	
+
+end
